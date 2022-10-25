43f3acca8cde8944bb3015afe06cd75b2b8267f6
Maks Novakov <maksnovakov78@gmail.com>
2022-10-25 19:29:17 +0300

    1,2 part

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,49 @@
+import css from '../Statistics/Statistics.module.css';
+import PropTypes from "prop-types"
+
+
+const createColor = () => {
+    const color = 'rgba(' +
+        Math.round(Math.random() * 255) +
+        ',' +
+        Math.round(Math.random() * 255) +
+        ',' +
+        Math.round(Math.random() * 255) +
+        ',' +
+        0.5 +
+        ')';
+    
+    return color;
+};
+
+export const Statistics = ({ title, stats }) => {
+    return (
+        <section className={css.statistics}>
+            {title && <h2 className={css.title}>{title}</h2>}
+
+            <ul className={css.stats__list}>
+                {stats.map(({ id, label, percentage }) => {
+                    return (
+                        <li className={css.stats}
+                            key={id}
+                            style={{ backgroundColor: createColor() }}
+                        >
+                            <span className={css.label}>{label}</span>
+                            <span className={css.percentage}>{percentage}</span>
+                        </li>
+                    );
+                })}
+            </ul>
+        </section>
+    );
+};
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(PropTypes.shape({
+        id:PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
+    }))
+  
+};
